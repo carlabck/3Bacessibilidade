3d620a4eb9b6564833a444b715295b5774f42aaa
carlabck <148265015+carlabck@users.noreply.github.com>
2025-04-28 09:08:17 -0300

    salvar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMcontentloaded',function(){
-const aumentaontBotao = document.getElementByid('aumentar-fonte');
+const aumentaFonteBotao = document.getElementByid('aumentar-fonte');
 
 let tamanhoAtualfonte = 1;
-aumentaFonteBotao.addEventListener('click', function(){
 
-tamanhoAtualfonte += 0.1;
-document.body.style.fontSize =`${tamanhoAtualfonte}rem`
+aumentaFonteBotao.addEventListener('click', function(){
+tamanhoAtualfonte += 0,1
+document.body.style.fontSize='${tamanhoAtualfonte}rem'
 })
 
 
